177ac836ec22fe866c2af16532ecec26375cef14
nabilahann <13519097@std.stei.itb.ac.id>
2023-06-07 09:21:10 +0700

    update code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, send_from_directory, jsonify
 from flask_ngrok import run_with_ngrok
 import os
+import threading
+
+from pyngrok import ngrok
+
 from werkzeug.utils import secure_filename
 from predict_data import predict
 from flask_cors import CORS
@@ -8,10 +12,19 @@ from flask_cors import CORS
 UPLOAD_FOLDER = '/content/be-ta/file'
 ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg'}
 
+os.environ["FLASK_ENV"] = "development"
+
 app = Flask(__name__)
 cors = CORS(app)
 run_with_ngrok(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Open a ngrok tunnel to the HTTP server
+public_url = ngrok.connect(5000).public_url
+print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, 5000))
+
+# Update any base URLs to use the public ngrok URL
+app.config["BASE_URL"] = public_url
 
 @app.route('/')
 def hello():
@@ -46,5 +59,5 @@ def predict_data(filename):
         'Access-Control-Allow-Origin': '*'
     } 
 
-if __name__ == '__main__':
-    app.run()
+# Start the Flask server in a new thread
+threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
